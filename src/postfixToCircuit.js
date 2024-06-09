d7eb25ad8cd481fec6294aabc3f121744a603799
istiakrifti <istiakrifti101@gmail.com>
2024-06-10 03:35:51 +0600

    commit2 from Rifti

--- src/postfixToCircuit.js
+++ src/postfixToCircuit.js
@@ -8,10 +8,22 @@ const PostfixEvaluator = () => {
   let disOfVar = 10;
   let heightofVar = 20;
   let stepDuration = 400;
+  let gateX = 400;
+  let gateY = 60;
+
   const svgMapOpt = {
-    '+': <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><line x1="8" y1="1" x2="8" y2="15" stroke="black" /><line x1="1" y1="8" x2="15" y2="8" stroke="black" /></svg>,
-    '*': <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><line x1="3" y1="3" x2="13" y2="13" stroke="black" /><line x1="3" y1="13" x2="13" y2="3" stroke="black" /></svg>,
-    '\'': <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><line x1="3" y1="3" x2="13" y2="13" stroke="black" /><line x1="3" y1="13" x2="13" y2="3" stroke="black" /></svg>
+    '*': `<path d="M 70 100 C 100 100, 100 140, 70 140 M 70 100 L 70 140" stroke="black" fill="transparent" stroke-width="2" />`,
+    '+': `<path d="M 70 100 C 105 105, 100 110, 115 127" stroke="black" fill="transparent" stroke-width="2" />
+    <path d="M 70 154 C 105 148, 100 143, 115 127" stroke="black" fill="transparent" stroke-width="2" />
+    <path d="M 70 100 C 80 110, 80 140, 70 154" stroke="black" fill="transparent" stroke-width="2" />`,
+    '\'': `<path d="M 10 80 L 45 105 L 10 135 Z" stroke="black" fill="transparent" stroke-width="2" />
+    <circle cx="50" cy="105" r="5" fill="none" stroke="black" stroke-width="2"/>`
+  };
+
+
+  const getPositionX = (char) => {
+    const variable = variables.find(variable => variable.name === char);
+    return variable ? variable.positionX : null;
   };
   
 
@@ -24,33 +36,63 @@ const PostfixEvaluator = () => {
     const tempStack = [];
     str += `<svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">`;
     for (let char of expression) {
-      if (char.length === 1 && char >= "A" && char <= "Z")
-        {
-          if(!variables.includes(char))
+      if (isOperand(char)) {
+        if(!variables.some(variable => variable.name === char))
           {
             str += 
             `<text x=${disOfVar} y="40" font-family="Verdana" font-size="25" fill="black">${char}</text>
-            <line x1=${disOfVar+7} y1="50" x2=${disOfVar+7} y2="1025" stroke="black" stroke-width="2" />
-            <line x1=${disOfVar+7} y1=${heightofVar+50} x2=${stepDuration} y2=${heightofVar+50} stroke="black" stroke-width="2" />
-            <circle cx=${disOfVar+7} cy=${heightofVar+50} r="4"/>
-          `;
-          
-          variables.push(char);
+            <line x1=${disOfVar+7} y1="50" x2=${disOfVar+7} y2="1025" stroke="black" stroke-width="2" />`;
+          const variable = {
+            name: char,
+            positionX: disOfVar + 7
+          }; 
+          variables.push(variable);
           disOfVar += 30;
-          heightofVar += 40;
           }
-        }
-      if (isOperand(char)) {
-        tempStack.push(char);
+          const stackVar = {
+            name: char,
+            position: 0,
+            positionX: getPositionX(char)
+          }; 
+        tempStack.push(stackVar);
+        
       } else{
         // Pop two operands from the stack
-        const svg = svgMapOpt[char];
+        // const svg = svgMapOpt[char]
+
         const operand2 = tempStack.pop();
         const operand1 = tempStack.pop();
+
+        const step = 1 + (operand1.position>=operand2.position ? operand1.position : operand2.position);
+
+        if(char === '*') 
+        {
+          str += `<path d="M ${gateX} ${gateY} C ${gateX+40} ${gateY}, ${gateX+40} ${gateY+50}, ${gateX} ${gateY+50} M ${gateX} ${gateY} L ${gateX} ${gateY+50}" stroke="black" fill="transparent" stroke-width="2" />`;
+          gateY += 80;
+        }
+
+        if(operand1.name.length ===1)
+        {
+          str += `<line x1=${operand1.positionX} y1=${heightofVar+50} x2=${stepDuration*step} y2=${heightofVar+50} stroke="black" stroke-width="2" />
+          <circle cx=${operand1.positionX} cy=${heightofVar+50} r="4"/>`
+          heightofVar += 30;
+        }
+        if(operand2.name.length ===1)
+        {
+          str += `<line x1=${operand2.positionX} y1=${heightofVar+50} x2=${stepDuration*step} y2=${heightofVar+50} stroke="black" stroke-width="2" />
+          <circle cx=${operand2.positionX} cy=${heightofVar+50} r="4"/>`
+          heightofVar += 30;
+        }
+        
         
         // Combine them with the operator into a string
-        const combined = `${operand1}${char}${operand2}`;
-        tempStack.push(combined);
+        const combined = `${operand1.name}${char}${operand2.name}`;
+        const stackVar = {
+          name: combined,
+          position: step,
+          positionX: 0
+        }; 
+        tempStack.push(stackVar);
         
       }
     }
@@ -72,6 +114,7 @@ const PostfixEvaluator = () => {
       <h3>Stack Content</h3>
       <div dangerouslySetInnerHTML={{ __html: elements }} />
     </div>
+    
   );
 };
 
